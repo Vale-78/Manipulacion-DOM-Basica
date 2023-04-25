0c4ecbd218f5fec0c4b995a3b300eeadbaad07c1
Valeria Foglino <valeriafoglino.78@gmail.com>
2023-04-25 17:44:27 -0300

    Agregar primeros archivos del proyecto

---
+++ Submit-form/script.js
@@ -0,0 +1,24 @@
+
+
+const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
+
+form.addEventListener('submit', sumarInputsValues);
+
+function sumarInputsValues(event) {
+    event.preventDefault(event);
+    const sumaInputs = "Hola usuario N° "+ input1.value + "." + " Bienvenida tu extraña palabra: " + input2.value;
+    pResult.innerText = "Resultado: " + sumaInputs;
+}
+
+// btn.addEventListener('click', sumarInputsValues);
+
+// function sumarInputsValues(event) {
+//     // event.preventDefault();
+//     const sumaInputs = input1.value + input2.value;
+//     pResult.innerText = "Resultado: " + sumaInputs;
+// }
